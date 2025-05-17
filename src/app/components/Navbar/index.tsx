6898b6292288c149dd2f25b5c70d7694f8ec3175
SakaDevs <zeooifa@gmail.com>
2025-05-17 13:16:37 +0700

    update lost

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { MagnifyingGlass, List, X } from "@phosphor-icons/react";
+import { List, X } from "@phosphor-icons/react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Poppins } from "next/font/google";
 import Cardscript from "./components/Card";
 
