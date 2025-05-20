c9ed2a3aecb8b1c350195b678b293fb580b2461d
SakaDevs <zeooifa@gmail.com>
2025-05-20 17:38:59 +0700

    update

--- src/app/components/Random/index.tsx
+++ src/app/components/Random/index.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import scripts from "@/app/components/Scripts/scripts.json";
 import Link from "next/link";
 import Image from "next/image";
 
+type Script = {
+  id: number;
+  title: string;
+  slug: string;
+  image: string;
+  date: string;
+};
+
 const Randomscript = () => {
-  const shuffled = [...scripts].sort(() => Math.random() - 0.5);
-  const latestScript = shuffled.slice(0, 8);
+  const [randomScripts, setRandomScripts] = useState<Script[]>([]);
+
+  useEffect(() => {
+    const shuffled = [...scripts].sort(() => Math.random() - 0.5);
+    const selected = shuffled.slice(0, 8);
+    setRandomScripts(selected);
+  }, []);
 
   return (
     <div className="flex flex-col gap-5 outline p-4 rounded">
@@ -13,7 +28,7 @@ const Randomscript = () => {
         <h3 className="font-bold md:text-lg text-sm">Random Script</h3>
       </div>
       <ul className="grid md:grid-cols-4 grid-cols-2 gap-4">
-        {latestScript.map((script) => (
+        {randomScripts.map((script) => (
           <li
             key={script.id}
             className="bg-white rounded shadow hover:shadow-lg transition overflow-hidden"
@@ -24,9 +39,9 @@ const Randomscript = () => {
                   src={script.image}
                   height={100}
                   width={300}
-                  alt=""
+                  alt={script.title}
                   className="rounded"
-                />{" "}
+                />
                 <div className="flex flex-col py-4 px-2">
                   <span className="text-xs font-thin">{script.date}</span>
                   <span className="md:text-base text-sm">{script.title}</span>
