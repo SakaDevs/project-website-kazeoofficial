b368b22237a3b73cfa98b3214ea1b5d78925ff9c
SakaDevs <zeooifa@gmail.com>
2025-05-27 18:21:16 +0700

    update page youtube

--- src/app/youtube/page.tsx
+++ src/app/youtube/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { Poppins } from "next/font/google";
+import PageWrapper from "../components/PageAnimation/page";
 
 type VideoData = {
   title: string;
@@ -38,40 +39,44 @@ export default function CommunityPage() {
 
   if (loading)
     return (
-      <p className="flex justify-center items-center text-center w-full">
+      <p
+        className={`${poppins.className} flex justify-center items-center text-center w-full h-screen`}
+      >
         Loading videos...
       </p>
     );
 
   return (
-    <div
-      className={`${poppins.className} p-6 flex flex-col text-center items-center`}
-    >
-      <h1 className="text-2xl font-bold mb-6">Video Terbaru</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-5xl h-screen">
-        {videos.map((video) => (
-          <div
-            key={video.videoId}
-            className="bg-white shadow-lg rounded-xl overflow-hidden hover:scale-102 transition-all"
-          >
-            <iframe
-              width="100%"
-              height="185"
-              src={`https://www.youtube.com/embed/${video.videoId}`}
-              title={video.title}
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              className="w-full"
-            ></iframe>
-            <div className="p-4 text-left">
-              <h2 className="font-semibold">{video.title}</h2>
-              <p className="text-sm text-gray-600">
-                Published: {new Date(video.publishedAt).toLocaleString()}
-              </p>
+    <PageWrapper>
+      <div
+        className={`${poppins.className} p-6 flex flex-col text-center items-center `}
+      >
+        <h1 className="text-2xl font-bold mb-6">Video Terbaru</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full max-w-9xl p-4 outline rounded">
+          {videos.map((video) => (
+            <div
+              key={video.videoId}
+              className="bg-white shadow-lg rounded-xl overflow-hidden hover:scale-102 transition-all"
+            >
+              <iframe
+                width="200"
+                height="170"
+                src={`https://www.youtube.com/embed/${video.videoId}`}
+                title={video.title}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                className="w-full"
+              ></iframe>
+              <div className="p-4 text-left">
+                <h2 className="font-semibold text-sm">{video.title}</h2>
+                <p className="text-sm text-gray-600">
+                  Published: {new Date(video.publishedAt).toLocaleString()}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </PageWrapper>
   );
 }
