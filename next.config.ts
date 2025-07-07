0582bf401869250ee79a81d17ba0943ca27864f1
SakaDevs <zeooifa@gmail.com>
2025-07-08 06:17:50 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,7 @@ const nextConfig: NextConfig = {
       "kazeoofficial.my.id",
       "res.cloudinary.com",
       "img.youtube.com",
+      "www.dearplayers.com",
     ],
   },
   async headers() {
